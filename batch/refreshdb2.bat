db360f6608979a0f7325df85f7cd08699420ba85
steve worlock <steve.worlock@capco.com>
2017-04-18 20:48:06 +0100

    jenkins integration windows batch

--- batch/refreshdb2.bat
+++ batch/refreshdb2.bat
@@ -1,5 +1,5 @@
-d c:\ror\eagleibank2\app\models
+cd c:\ror\eagleibank2\app\models
 del transaction.rb
 copy transaction_backup.rb transaction.rb
 cd c:\ror\eagleibank2
-echo "moel dependencies reestablished"
+echo "model dependencies reestablished"
