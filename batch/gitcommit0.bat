900fed9683ed536d1123d084f8d8ae268ac233e4
steve worlock <steve.worlock@capco.com>
2017-05-06 02:27:02 +0100

    'stuff'

---
+++ batch/gitcommit0.bat
@@ -0,0 +1,11 @@
+REM Select the correct directory to issue rake command to re-instantiate databases
+cd c:\ror\eagleibank2
+git status
+git add *.*
+git remote -v
+git remote rm origin
+git remote add arigin https://steve-worlock/eagle-nursery
+git remote -v
+git commit -m ‘Updated build number’
+git push
+
