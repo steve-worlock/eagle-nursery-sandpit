bbd95f0be92b64f828ba06e400bd689fbc25d531
steve worlock <steve.worlock@capco.com>
2017-05-06 02:45:26 +0100

    test

--- batch/gitcommit0.bat
+++ batch/gitcommit0.bat
@@ -1,4 +1,5 @@
 REM Select the correct directory to issue rake command to re-instantiate databases
+REM
 cd c:\ror\eagleibank2
 git status
 git add *.*
@@ -7,5 +8,5 @@ git remote rm origin
 git remote add arigin https://steve-worlock/eagle-nursery
 git remote -v
 git commit -m ‘Updated build number’
-git push
+git push -u origin master
 
