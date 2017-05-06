e553da596f374900502be7f2ac68de2f6e17da2e
steve worlock <steve.worlock@capco.com>
2017-05-06 02:52:52 +0100

    Updated build number

---
+++ batch/gitcommit.bat
@@ -0,0 +1,9 @@
+REM Select the correct directory to issue rake command to re-instantiate databases
+REM
+cd c:\ror\eagleibank
+git status
+git add *.*
+git remote -v
+git commit -m "Updated build number"
+git push -u origin master
+
--- batch/gitcommit2.bat
+++ batch/gitcommit2.bat
@@ -4,9 +4,6 @@ cd c:\ror\eagleibank2
 git status
 git add *.*
 git remote -v
-git remote rm origin
-git remote add arigin https://steve-worlock/eagle-nursery
-git remote -v
-git commit -m ‘Updated build number’
+git commit -m "Updated build number"
 git push -u origin master
 
